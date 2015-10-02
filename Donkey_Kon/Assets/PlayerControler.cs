95ec0d0bdfa05e0e00fd67b1d0bb12756685f714
laurensWe <laurensweijs@hotmail.com>
2015-10-02 11:40:08 +0200

    Script beginning & scene adjustments

---
+++ Donkey_Kon/Assets/PlayerControler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        // horizontal move
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        // jump
+        bool moveUp = Input.GetButton("Jump");
+        float amountMoveUp;
+        if (moveUp)
+        { 
+            amountMoveUp = 3;
+        }
+        else
+        {
+            amountMoveUp = 0;
+        }
+
+        // 
+        Vector2 movement = new Vector2 (moveHorizontal,amountMoveUp);
+
+    }
+}
