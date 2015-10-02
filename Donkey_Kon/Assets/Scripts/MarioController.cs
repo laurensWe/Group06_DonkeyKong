e78d0879cbab71c279a7c41d99417f832f911d97
JarcodeJong <jarco1995@hotmail.com>
2015-10-02 15:44:30 +0200

    Mario flipping enabled

--- Donkey_Kon/Assets/Scripts/MarioController.cs
+++ Donkey_Kon/Assets/Scripts/MarioController.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class MarioController : MonoBehaviour
 {
     public float speed;
+    bool facingRight = true;
 
     private Rigidbody2D rb;
 
@@ -25,16 +26,32 @@ public class MarioController : MonoBehaviour
         float amountMoveUp;
         if (moveUp)
         {
-            amountMoveUp = 1;
+            amountMoveUp = 2;
         }
         else
         {
             amountMoveUp = 0;
         }
 
-        // 
+        
         Vector2 movement = new Vector2(moveHorizontal, amountMoveUp);
         rb.AddForce(movement * speed);
 
+        if(moveHorizontal>0 &&!facingRight)     //If the player moves right but is not facing right yet, flip the player
+        {
+            Flip();
+        }
+        else if(moveHorizontal<0 && facingRight) 
+        {
+            Flip();
+        }
+
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;        //Get the local Scale
+        theScale.x *= -1;                               //Flip the x axis
+        transform.localScale = theScale;                //Apply the new local Scale
     }
 }
