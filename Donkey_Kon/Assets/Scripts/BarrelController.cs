44bbdf231f1fd8dfa8dc0ceb72a0205c70d985a7
laurensWe <laurensweijs@hotmail.com>
2015-10-06 11:34:00 +0200

    Latest version

---
+++ Donkey_Kon/Assets/Scripts/BarrelController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrelController : MonoBehaviour {
+
+    public float speed;
+    public Vector2 direction;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+	}
+
+    // Update is called once per frame
+    void Update() {
+        rb.velocity = new Vector2(speed * direction.x, speed * direction.y);
+
+	}
+
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.CompareTag("ChangeDirection"))
+        {
+            direction = new Vector2(-1 * direction.x, direction.y);
+
+        }
+    }
+}
--- Donkey_Kon/Assets/Scripts/BarrelManager.cs
+++ Donkey_Kon/Assets/Scripts/BarrelManager.cs
@@ -8,23 +8,21 @@ public class BarrelManager : MonoBehaviour {
     public float spawnTime = 3f;
     public Transform[] spawnPoints;
 
-
 	// Use this for initialization
 	void Start () {
         InvokeRepeating("Spawn", spawnTime, spawnTime);
 	}
 
+
     // Update is called once per frame
     void Spawn()
     {
-
         // should be added when playerhealth is added to the program.
 //        if (playerHealth.currentHealth <= 0f)
 //        {
 //            return;
 //        }
         int spawnPointIndex = Random.Range(0, spawnPoints.Length);
-
         Instantiate(barrel, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
 	}
 }
