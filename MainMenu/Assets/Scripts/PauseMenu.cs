e95b637b7470b79885b83a9426ddcc84063298d0
Mats <M.Mulder-5@student.tudelft.nl>
2015-10-09 12:11:00 +0200

    Pauze Menu

---
+++ MainMenu/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public string MainMenu;
+    public bool isPaused;
+    public GameObject pauseMenuCanvas;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPaused)
+        {
+            pauseMenuCanvas.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            pauseMenuCanvas.SetActive(false);
+            Time.timeScale = 1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            isPaused = !isPaused;
+
+        }
+
+    }
+
+    public void resume()
+    {
+        isPaused = false;
+    }
+
+    public void QuiteGame()
+    {
+        Application.LoadLevel(MainMenu);
+    }
+}
