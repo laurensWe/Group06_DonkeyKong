a2ed4a83451e6a09297477cd7ce7751bafac49ee
laurensWe <laurensweijs@hotmail.com>
2015-10-09 09:44:09 +0200

    Score manager

---
+++ MainMenu/Assets/ScoreManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public static int score;
+
+    public Text text;
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = "Score: " + score;
+	}
+}
