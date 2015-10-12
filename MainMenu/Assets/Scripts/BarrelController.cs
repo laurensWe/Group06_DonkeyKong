6ba0e00920a046fa9fc58ffee53e5e11a5318adf
laurensWe <laurensweijs@hotmail.com>
2015-10-12 19:35:31 +0200

    Scripts addes for barrels and barrelmanager

--- MainMenu/Assets/Scripts/BarrelController.cs
+++ MainMenu/Assets/Scripts/BarrelController.cs
@@ -1,70 +1,161 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BarrelController : MonoBehaviour {
+public class BarrelController : MonoBehaviour
+{
 
     public float speed;
     public Vector2 direction;
+    private Vector2 tempDirection;
 
     private Rigidbody2D rb;
 
     public Transform JumpCheck;
     public float JumpCheckRadius;
-    public LayerMask PlayerLayer;
 
     public Animator anim;
+    private int random;
+
+    private bool onLadder;
+    private bool onGround;
+
+    private float Timer1;
+    private float Timer2;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
-
-	}
+        
+    }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
+        if (Time.time > Timer1)      
+        {
+            random = Random.Range(1,10);  
+            Timer1 = Time.time + 1;
+     
+        }
 
-            rb.velocity = new Vector2(speed * direction.x, speed * direction.y);
+        // JumpCheck update
         if (JumpCheck != null)
         {
             JumpCheck.position = new Vector2(this.transform.position.x, this.transform.position.y + 0.75f);
+
         }
-	}
 
-    void OnTriggerEnter2D(Collider2D other){
+        // ignore the jumpcheck collision
+        var ladders = GameObject.FindGameObjectsWithTag("Ladder");
+        foreach (var ladder in ladders)
+            Physics2D.IgnoreCollision(JumpCheck.GetComponent<Collider2D>(), ladder.GetComponent<Collider2D>(), true);
 
+
+        // with a chance of 50% the barrel wil go down the ladder.
+        if (!onLadder)
+        {
+            rb.velocity = new Vector2(speed * direction.x, speed * direction.y);
+            anim.SetBool("OnLadder", false);
+            
+            var ladderbars = GameObject.FindGameObjectsWithTag("BarWithLadder");
+            foreach (var ladderbar in ladderbars)
+                Physics2D.IgnoreCollision(rb.GetComponent<Collider2D>(), ladderbar.GetComponent<Collider2D>(), false);
+        }
+        else
+        { 
+            if (!onGround && random < 5)
+            {
+                
+                if (Time.time > Timer2)
+                {
+                    anim.SetBool("OnLadder", true);
+                    rb.velocity = new Vector2(speed*direction.x, speed* 0.001f * direction.y - 10);
+
+                    var ladderbars = GameObject.FindGameObjectsWithTag("BarWithLadder");
+                    foreach (var ladderbar in ladderbars)
+                        Physics2D.IgnoreCollision(rb.GetComponent<Collider2D>(), ladderbar.GetComponent<Collider2D>(), true);
+
+                    Timer2 = Time.time + 1;
+                    direction.x = -1 * direction.x;
+                }
+                
+            }
+            else
+            {
+                anim.SetBool("OnLadder", false);
+ 
+                rb.velocity = new Vector2(speed * direction.x, speed * direction.y);
+
+                var ladderbars = GameObject.FindGameObjectsWithTag("BarWithLadder");
+                foreach (var ladderbar in ladderbars)
+                    Physics2D.IgnoreCollision(rb.GetComponent<Collider2D>(), ladderbar.GetComponent<Collider2D>(), false);
+            }
+        }
+
+    }
+
+    // checks if a trigger has entered the barrel collider
+    void OnTriggerEnter2D(Collider2D other)
+    {
 
         if (other.CompareTag("ChangeDirection"))
         {
-            direction = new Vector2(-1 * direction.x, direction.y - 0.5f);
+            direction = new Vector2(-1 * direction.x, direction.y - 1f);
 
         }
 
         // Barrel reaches the end of the level
-        if(other.CompareTag("Destroy"))
+        if (other.CompareTag("Destroy"))
         {
             Destroy(gameObject);
         }
 
         // Barrel collides with mario
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
             // play animation of death and open Game Over Menu. 
         }
-        
-        // Barrel collides with Ladder
-       // if(other.CompareTag("Ladder"))
-       // {
-            // with a probability of 50 % barrel will go down the ladder.
-           // if(Random.Range(0,1) < 0.5)
-           // {
-               // anim.SetBool("OnLadder", true);
-               // rb.velocity = new Vector2(speed * rb.position.x, speed * direction.y);
 
-           // }
+        //Barrel collides with Ladder
+        if (other.CompareTag("Ladder"))
+        {
+            onLadder = true;
 
-       // }
+        }   
 
-        //anim.SetBool("OnLadder", false);
     }
- 
+
+    // checks if a trigger has left the barrel collider
+    void OnTriggerExit2D(Collider2D other)
+    {
+
+        if (other.CompareTag("Ladder"))
+        {
+            onLadder = false;
+
+        }
+
+    }
+
+    // checks if a collider has entered the barrel collider
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Bar"))
+        {
+            onGround = true;
+        }
+    }
+
+    // checks if a collider has left the barrel collider
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Bar"))
+        {
+            onGround = false;
+        }
+    }
+
+
+
 }
--- MainMenu/Assets/Scripts/BarrelManager.cs
+++ MainMenu/Assets/Scripts/BarrelManager.cs
@@ -5,29 +5,25 @@ public class BarrelManager : MonoBehaviour {
 
     //public PlayerHealth playerHealth;
     public GameObject barrel;
-    public float spawnTime = 3f;
+    public float spawnTime;
     public Transform[] spawnPoints;
 
-	// Use this for initialization
-	void Start () {
-        InvokeRepeating("Spawn", spawnTime, spawnTime);
-
-	}
-
-
-    // Update is called once per frame
-    void Spawn()
-    {
-        // should be added when playerhealth is added to the program.
-//        if (playerHealth.currentHealth <= 0f)
-//        {
-//            return;
-//        }
-        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
-        Instantiate(barrel, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
-
-	}
-
+    private float Timer;
     
+
+
+	void Start () {
+ 
+    }
+
+    void Update()
+    {
+        if (Timer < Time.time)
+        {
+            int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+            Instantiate(barrel, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+            Timer = Time.time + Random.Range(2, 5);
+        }
+    }
 
 }
