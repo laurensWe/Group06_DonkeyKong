c8e7bd09e77e26094bcaa4c39c604cef539a708e
Mats <M.Mulder-5@student.tudelft.nl>
2015-10-14 20:47:07 +0200

    Muziek

--- MainMenu/Assets/Scripts/DonkeyController.cs
+++ MainMenu/Assets/Scripts/DonkeyController.cs
@@ -3,26 +3,31 @@ using System.Collections;
 
 public class DonkeyController : MonoBehaviour {
 
-    private bool doSomething;
+    public bool Action;
     private float randomNumber;
-
+    private float Timer1;
+    private Animator anim;
     // public Animator anim;
 
     // Use this for initialization
     void Start () {
-        // anim.GetComponent<Animator>();
+        anim.GetComponent<Animator>();
+        anim.SetBool("Action", Action);
+        Timer1 = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // make donkey do some action moves at a random time interval
-       // randomNumber = Random.Range(0, 1);
-       // if (randomNumber < 0.2)
-        //{
-            //anim.SetBool("Action", true);
-        //}
-    //
+        Timer1 = Timer1 + 1;
+        if(Timer1 > 100) {
+            randomNumber = Random.Range(0, 1);
+            if (randomNumber < 1)
+            {
+                Debug.Log("hallo");
+            anim.SetBool("Action", Action);
+            }
+    }
         //anim.SetBool("Action", false);
     }
 }
