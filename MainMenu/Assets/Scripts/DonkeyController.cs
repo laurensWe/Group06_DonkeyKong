295d19646837bffd33b142279c5af0c84449344b
Mats <M.Mulder-5@student.tudelft.nl>
2015-10-14 22:28:33 +0200

    Donkey animation opgestart

--- MainMenu/Assets/Scripts/DonkeyController.cs
+++ MainMenu/Assets/Scripts/DonkeyController.cs
@@ -5,29 +5,26 @@ public class DonkeyController : MonoBehaviour {
 
     public bool Action;
     private float randomNumber;
-    private float Timer1;
-    private Animator anim;
+    private int Timer1;
+    public Animator anim;
     // public Animator anim;
 
     // Use this for initialization
     void Start () {
         anim.GetComponent<Animator>();
-        anim.SetBool("Action", Action);
         Timer1 = 0;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Timer1 = Timer1 + 1;
-        if(Timer1 > 100) {
-            randomNumber = Random.Range(0, 1);
-            if (randomNumber < 1)
-            {
-                Debug.Log("hallo");
+        randomNumber = Random.Range(1, 100);
+        if (randomNumber <= 2)
+        {
+            Action = true;
             anim.SetBool("Action", Action);
-            }
-    }
-        //anim.SetBool("Action", false);
+            Timer1 = 1;
+        }
+            
+        
     }
 }
