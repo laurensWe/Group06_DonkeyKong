7aa0d8f1548c722345cbb2873bafc7b1885009ec
JarcodeJong <jarco1995@hotmail.com>
2015-10-15 13:15:35 +0200

    Lady Pauline animation

--- MainMenu/Assets/Scripts/PeachController.cs
+++ MainMenu/Assets/Scripts/PeachController.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class PeachController : MonoBehaviour {
 
     public int ScoreKeeper;
+    private bool GameWon;
+    private Animator anim;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        anim = GetComponent<Animator>();    
 	}
 
     void OnTriggerEnter2D(Collider2D other)
@@ -15,12 +19,31 @@ public class PeachController : MonoBehaviour {
 
         if (other.CompareTag("Player"))
         {
-            
-            // play animation that you'll receive points
-            Destroy(gameObject);
-            ScoreKeeper = ScoreManager.score;
-            Application.LoadLevel("Game_Won");
 
+            // play animation that you'll receive points
+            //Destroy(gameObject);
+            ScoreKeeper = ScoreManager.score;
+            //StartCoroutine("wait");
+
+            GameWon = true;
         }
+    }
+
+
+    void Update()
+    {
+        anim.SetBool("GameWon", GameWon);
+        if (GameWon==true)
+        {
+            StartCoroutine("wait");
+        }
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(2);
+        Destroy(gameObject);
+        Application.LoadLevel("Game_Won");
+        
     }
 }
