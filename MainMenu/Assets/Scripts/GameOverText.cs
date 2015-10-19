c03e7449ae1f1d198958365882a84e7a90565da7
JarcodeJong <jarco1995@hotmail.com>
2015-10-19 11:34:44 +0200

    Highscore to main menu added

---
+++ MainMenu/Assets/Scripts/GameOverText.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameOverText : MonoBehaviour {
+    public Text HighScore;
+
+    public void Update()
+    {
+        HighScore.text = "HighScore: " + PlayerPrefs.GetInt("highScore");
+    }
+}
